8c26ca18e69d13cb81be7350c6935a6c40854858
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-15 18:37:55 -0500

    Componente Navbar terminado

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.styles.scss';
-import logo from '../../assets/logo.png';
+import Logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 const Navbar = () => {
+    const [openLinks, setOpenLinks] = useState(false);
+  
+    const toggleNavbar = () => {
+      setOpenLinks(!openLinks);
+    };
     return(
         <div className="navbar">
-            <div className="leftSide">
-                <img src={logo} />
+            <div className="leftSide" id={openLinks ? "open" : "close"}>
+                <img src={Logo} />
+
+                <div className="hiddenLinks">
+                    <Link to="/" >Home</Link>
+                    <Link to="/" >Menu</Link>
+                    <Link to="/" >About</Link>
+                    <Link to="/" >Contact</Link>
+                </div>
             </div>
             
             <div className="rightSide">
@@ -15,6 +28,9 @@ const Navbar = () => {
                 <Link to="/" >Menu</Link>
                 <Link to="/" >About</Link>
                 <Link to="/" >Contact</Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon/>
+                </button>
             </div>
         </div>
     );
