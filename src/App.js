4e46ab9dc62ef1d7da7919b3c5353bf254c6cf9f
patricio0312rev <review0312oficial@gmail.com>
2022-01-15 17:53:30 -0500

    Configurando Router en App.js y configurando Links en componente Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+
 import Navbar from './components/navbar/navbar.component';
+import { BrowserRouter as Router, Switch, Route,  } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
