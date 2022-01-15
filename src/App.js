8e953cafbe29abff396c84bbcdb6bcb875a639bc
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-15 18:51:09 -0500

    Creando el componente HomePage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
-
 import Navbar from './components/navbar/navbar.component';
-import { BrowserRouter as Router, Switch, Route,  } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home/home.component';
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Navbar />
-      </Router>
+        
+        <Routes>
+          <Route path="/home" exact element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
