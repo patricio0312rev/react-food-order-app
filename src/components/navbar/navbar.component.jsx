4cc3941d95482d15e72171a258727a28d1d21ee5
patricio0312rev <review0312oficial@gmail.com>
2022-01-15 17:43:35 -0500

    Instalando SASS, creando folder components & creando componente Navbar

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navbar.styles.scss';
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <div className="leftSide">
+                leftSide
+            </div>
+            
+            <div className="rightSide">
+                rightSide
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
