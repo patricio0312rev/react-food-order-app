11d65382c5853aadf8ab0e7441c6d11f28dd57a3
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-15 19:36:30 -0500

    Creando el componente ContactPage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/footer.component';
 import HomePage from './pages/home/home.component';
 import MenuPage from './pages/menu/menu.component';
 import AboutPage from './pages/about/about.component';
+import ContactPage from './pages/contact/contact.component';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" exact element={<HomePage />} />
           <Route path="/menu" exact element={<MenuPage />} />
           <Route path="/about" exact element={<AboutPage />} />
+          <Route path="/contact" exact element={<ContactPage />} />
         </Routes>
 
         <Footer />
